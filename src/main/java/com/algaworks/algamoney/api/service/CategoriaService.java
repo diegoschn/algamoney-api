74dfdc307920656c5063721fefd60e77bc344a32
Diego Schneider <diegoschneiderssa@gmail.com>
2020-08-19 03:48:06 -0300

    Criação das camadas repository,service,model,e metodos crud

---
+++ src/main/java/com/algaworks/algamoney/api/service/CategoriaService.java
@@ -0,0 +1,17 @@
+package com.algaworks.algamoney.api.service;
+
+import com.algaworks.algamoney.api.model.Categoria;
+import com.algaworks.algamoney.api.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria salvar(Categoria categoria){
+        return categoriaRepository.save(categoria);
+    }
+}
