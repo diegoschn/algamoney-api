d80f7a8ad8bfaaeca6d8fa4af8cb29522d4668e9
Diego Schneider <diegoschneiderssa@gmail.com>
2020-09-08 11:33:58 -0300

    Adicionado jwt token

--- src/main/java/com/algaworks/algamoney/api/model/Categoria.java
+++ src/main/java/com/algaworks/algamoney/api/model/Categoria.java
@@ -5,6 +5,8 @@ import org.hibernate.validator.constraints.NotBlank;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "categoria")
